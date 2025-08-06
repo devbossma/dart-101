216b1a5ff72ce96e0277e735b072914a0af532a6
devbosslocal <ysaber201@outlook.com>
2025-08-06 23:02:26 +0100

    Adding Dart variables file

--- 101.dart
+++ 101.dart
@@ -1,4 +1,9 @@
 void main () {
   // This is a simple Dart program that prints "Hello, World!" to the console.
+  /**
+   * Every app requires the top-level main() function, where execution starts.
+   * Functions that don't explicitly return a value have the void return type.
+   * To display text on the console, you can use the top-level print() function:
+   */
   print('Hello, World!');
 }
